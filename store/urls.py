90e1c90f853bda11e81584544857f7ecbf835968
Aleksandr Smelik <suntrope55@gmail.com>
2022-11-03 20:08:22 +0600

    Change DB on MySQL, create model Tag, Change content in Template

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('category/<slug>', views.ProductsByCategoryView.as_view(), name='products_category'),
     path('detail/<int:pk>', views.SingleProducDetailView.as_view(), name='single_product'),
+    path('tag/<slug>/', views.ProductsByTagsView.as_view(), name='products_tag'),
     path('contact/', views.contact, name='contact'),
 ]
